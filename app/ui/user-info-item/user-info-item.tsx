e8a43229917a41b97def7695900c8afd37bc2b1e
Monireh <monireamini@gmail.com>
2024-08-09 22:13:30 +0330

    remove unused component

--- app/ui/user-info-item/user-info-item.tsx
+++ app/ui/user-info-item/user-info-item.tsx
@@ -9,7 +9,7 @@ export const UserInfoItem = ({
     key?: string
 }) => {
     return (
-        <div className='flex w-full items-start justify-between gap-2 border-b border-gray-50 py-4'>
+        <div className='flex w-full items-start justify-between gap-2 border-gray-50 py-4'>
             <div className='w-100 flex flex-col'>
                 <p className='text-xs'>{title}</p>
                 <p className='font-medium'>{value}</p>
