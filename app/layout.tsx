ca818419b77b964a1c846ddd3c818f7e9cdf2fd7
Monireh <monireamini@gmail.com>
2024-08-09 14:39:53 +0330

    add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import {dmsans} from "@/app/ui/fonts";
-
+import type { Metadata } from 'next'
+import './globals.css'
+import { dmsans } from '@/app/ui/fonts'
 
 export const metadata: Metadata = {
-  title: "Karnameh Interview",
-  description: "Dynamic Dashboard",
-};
+    title: 'Karnameh Interview',
+    description: 'Dynamic Dashboard',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${dmsans.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${dmsans.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    )
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
-import { DM_Sans } from 'next/font/google';
+import { DM_Sans } from 'next/font/google'
 
-export const dmsans = DM_Sans({ subsets: ['latin'] });
+export const dmsans = DM_Sans({ subsets: ['latin'] })
